e8cfd9d5bf6fe6c1a00980cda994c8d78d6c762d
Jordi Riera <jordi.riera@numigi.com>
2018-03-14 21:18:26 -0400

    add updoo + tests to confirm commands are available

---
+++ addons/test_suites/README.rst
@@ -0,0 +1,8 @@
+Main Module
+===========
+This module contains the dependencies to install all others modules
+
+Contributors
+------------
+* Jordi Riera
+* David Dufresne
---
+++ addons/test_suites/tests/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# © 2018 Jordi Riera
+# © 2018 David Dufresne
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from . import test_linux_commands
