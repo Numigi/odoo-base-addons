9333b338433baec721d8ede36dd0032246d25f8e
Jordi Riera <547282+foutoucour@users.noreply.github.com>
2018-09-17 10:39:14 -0400

    add ir_attachment_name_autocomplete (TA#4926) (#1)
    * add ir_attachment_name_autocomplete (TA#4926)

---
+++ ir_attachment_name_autocomplete/models/ir_attachment.py
@@ -0,0 +1,27 @@
+# © 2018 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from odoo import api, models
+
+
+class IrAttachmentNameAutoComplete(models.Model):
+    """ Set the name of the attachment if the name is not set."""
+    _inherit = 'ir.attachment'
+
+    @api.onchange('datas')
+    def _compute_name(self):
+        """ As soon as a file is uploaded by the user, the name is updated
+        using the name if the file.
+
+        Known weakness:
+            * hard to test as the behaviour between the api and the GUI is different.
+                The name is required as soon as we want to create the attachment from the API.
+            * if the user re-upload, the name won't be updated again, and the name of
+                old file will stays.
+        """
+        nameless_attachments = (
+            attachment for attachment in self
+            if not attachment.name
+        )
+        for attachment in nameless_attachments:
+            attachment.name = attachment.datas_fname
