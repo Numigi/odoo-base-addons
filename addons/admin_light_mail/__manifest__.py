597feb782195a8ed464b8cd3b9b1fe4649baa144
David Dufresne <david.dufresne@numigi.net>
2018-09-24 15:21:31 -0400

    Add module admin_light_mail

---
+++ addons/admin_light_mail/__manifest__.py
@@ -0,0 +1,22 @@
+# © 2018 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    'name': 'Admin Light Email',
+    'version': '1.0.0',
+    'author': 'Numigi',
+    'maintainer': 'Numigi',
+    'license': 'LGPL-3',
+    'category': 'Other',
+    'summary': 'Add email management to the Admin Light application.',
+    'depends': ['admin_light_base', 'auditlog', 'fetchmail'],
+    'data': [
+        'common.xml',
+        'email_and_messages.xml',
+        'mail_template.xml',
+        'mail_server.xml',
+        'security/ir.model.access.csv',
+    ],
+    'installable': True,
+    'application': True
+}
