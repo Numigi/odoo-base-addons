0fb3f4c0651913aa4a138a22b538f9434f4e19ec
David Dufresne <david.dufresne@numigi.net>
2018-09-25 16:53:15 -0400

    Add module admin_light_calendar

--- numipack/__manifest__.py
+++ numipack/__manifest__.py
@@ -13,6 +13,7 @@
     'depends': [
         # Numigi/odoo-base
         'admin_light_auditlog',  # TA#3892
+        'admin_light_calendar',  # TA#3892
         'admin_light_mail',  # TA#3892
         'admin_light_base',  # TA#4894
         'admin_light_user',  # TA#4894
