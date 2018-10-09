6181dc466b6bb0b947b2aebe4ceb8aee534eb1d6
Jordi Riera <jordi.riera@numigi.com>
2018-10-09 17:44:17 -0400

    remove unported modules

--- numipack/__manifest__.py
+++ numipack/__manifest__.py
@@ -19,32 +19,32 @@
         'admin_light_user',  # TA#4894
 
         # OCA/server-tools
-        'base_technical_features',
+        # 'base_technical_features',
 
         # OCA/server-ux
-        'mass_editing',  # TA#4119
+        # 'mass_editing',  # TA#4119
 
         # odoo/odoo
         'board',
         'document',  # TA#3415
 
         # OCA/server_brand
-        'disable_odoo_online',
+        # 'disable_odoo_online',
 
         # OCA/server-auth
-        'password_security',  # TA#2532
+        # 'password_security',  # TA#2532
 
         # Numigi/aeroo_reports
-        'report_aeroo',
+        # 'report_aeroo',
 
         # Numigi/odoo-web-addons
-        'web_contextual_search_favorite',  # TA#2637
-        'web_custom_label',  # TA#3928
-        'web_search_date_range',
-        'disable_quick_create',
+        # 'web_contextual_search_favorite',  # TA#2637
+        # 'web_custom_label',  # TA#3928
+        # 'web_search_date_range',
+        # 'disable_quick_create',
 
         # OCA/web
-        'web_search_with_and',  # TA#2547
+        # 'web_search_with_and',  # TA#2547
     ],
     'data': [
         'admin_light_user_password_security_binding.xml',
