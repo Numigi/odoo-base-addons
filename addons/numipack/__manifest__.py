50d57bd1e810b2f01b1d0ce19dc7c70a2f39f0af
Jordi Riera <jordi.riera@numigi.com>
2018-10-09 17:51:26 -0400

    remove unported modules

--- addons/numipack/__manifest__.py
+++ addons/numipack/__manifest__.py
@@ -12,7 +12,7 @@
     'summary': 'Functional dependencies for all Odoo instances.',
     'depends': [
         # Numigi/odoo-base
-        'admin_light_auditlog',  # TA#3892
+        # 'admin_light_auditlog',  # TA#3892
         'admin_light_calendar',  # TA#3892
         'admin_light_mail',  # TA#3892
         'admin_light_base',  # TA#4894
