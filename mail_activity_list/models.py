91d8ca6d3d471722b8881c7b0ccba3603cc663c4
David Dufresne <27902736+ddufresne@users.noreply.github.com>
2019-03-07 10:36:57 -0500

    mail_activity_list: Set activity display name to  when no summary (#14)

--- mail_activity_list/models.py
+++ mail_activity_list/models.py
@@ -1,7 +1,7 @@
 # © 2019 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
-from odoo import api, fields, models
+from odoo import api, fields, models, _
 
 
 class ActivityWithRecordReference(models.Model):
@@ -28,3 +28,16 @@ class ActivityWithEffectiveDate(models.Model):
     def _compute_effective_date(self):
         for activity in self:
             activity.effective_date = activity.date_done or activity.date_deadline
+
+
+class ActivityWithoutFalseInDisplayName(models.Model):
+    """Prevent an activity with the display name False."""
+
+    _inherit = 'mail.activity'
+
+    effective_date = fields.Date(compute='_compute_effective_date', store=True)
+
+    @api.multi
+    def name_get(self):
+        default_name = _('Activity')
+        return [(a.id, a.summary or default_name) for a in self]
