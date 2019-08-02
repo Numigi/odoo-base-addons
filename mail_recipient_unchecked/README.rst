8af9889426b3a671517e0d26b0ea5517d69d1eab
David Dufresne <27902736+ddufresne@users.noreply.github.com>
2019-08-02 14:35:37 -0400

    12.0 mig mail recipient unchecked (#24)
    Squash merge pull request MDExOlB1bGxSZXF1ZXN0MzAzODU4NzM0

---
+++ mail_recipient_unchecked/README.rst
@@ -0,0 +1,11 @@
+Mail Recipient Unchecked
+========================
+In vanilla Odoo, the mail recipient checkbox is checked by default.
+
+.. image:: static/description/mail_recipient_checkbox.png
+
+With this module installed, the checkbox is unchecked by default.
+
+Contributors
+------------
+* Numigi (tm) and all its contributors (https://bit.ly/numigiens)
---
+++ mail_recipient_unchecked/__manifest__.py
@@ -0,0 +1,17 @@
+# © 2018 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    'name': 'Mail Recipient Unchecked',
+    'version': '1.0.0',
+    'author': 'Numigi',
+    'maintainer': 'Numigi',
+    'license': 'LGPL-3',
+    'category': 'Other',
+    'summary': 'Uncheck the recipients of chatter message by default.',
+    'depends': [
+        'mail',
+    ],
+    'qweb': ['templates/chatter.xml'],
+    'installable': True,
+}
