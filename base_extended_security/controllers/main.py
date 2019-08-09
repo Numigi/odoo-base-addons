b8011b7f2b01a23990bf594c133cb1ad3eface52
David Dufresne <27902736+ddufresne@users.noreply.github.com>
2019-08-09 12:16:25 -0400

    Apply access rules to /web/export controller

--- base_extended_security/controllers/main.py
+++ base_extended_security/controllers/main.py
@@ -1,6 +1,7 @@
 # © 2019 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
+import json
 from odoo.addons.web.controllers.main import DataSet
 from odoo.http import request
 from odoo.osv.expression import AND
