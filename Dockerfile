acd1086771612660e6dec09e6a0b99feed4ef916
David Dufresne <27902736+ddufresne@users.noreply.github.com>
2019-08-17 08:59:19 -0400

    11.0 disable install from website (#30)
    * add disable_install_from_website (TA#12714) (#19)
    
    Squash merge pull request MDExOlB1bGxSZXF1ZXN0Mjg3NjUwOTQw
    
    * Backport module disable_install_from_website to 11.0

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,7 @@
 FROM quay.io/numigi/odoo-public:11.0
 MAINTAINER numigi <contact@numigi.com>
 
+COPY disable_install_from_website /mnt/extra-addons/disable_install_from_website
 COPY ir_attachment_access_token_portal /mnt/extra-addons/ir_attachment_access_token_portal
 COPY ir_attachment_name_autocomplete /mnt/extra-addons/ir_attachment_name_autocomplete
 COPY mail_activity_list /mnt/extra-addons/mail_activity_list
---
+++ disable_install_from_website/ir_module.py
@@ -0,0 +1,22 @@
+# © 2019 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+import logging
+from odoo import models, api, _
+from odoo.addons.base.module.module import assert_log_admin_access
+from odoo.exceptions import UserError
+
+_logger = logging.getLogger(__name__)
+
+
+class Module(models.Model):
+
+    _inherit = 'ir.module.module'
+
+    @assert_log_admin_access
+    @api.multi
+    def button_immediate_install(self):
+        """ Disable install of selected modules.
+        """
+        raise UserError(_("Install of modules through the interface is disable. "
+                          "Contact your Odoo Partner for more details."))
