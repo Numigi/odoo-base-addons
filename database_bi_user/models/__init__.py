f81259c1771767e96b2e90f22c22ca643158683e
David Dufresne <27902736+ddufresne@users.noreply.github.com>
2019-09-03 10:12:54 -0400

    Add module database_bi_user (#177)

---
+++ database_bi_user/models/__init__.py
@@ -0,0 +1,7 @@
+# © 2019 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from . import (
+    database_bi_user_update,
+    res_config_settings,
+)
