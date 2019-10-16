0d5fbed6474651cf87f8fe01062f406004f23505
David Dufresne <27902736+ddufresne@users.noreply.github.com>
2019-10-16 11:29:00 -0400

    Improve mock_odoo_request (#38)
    * Improve mock_odoo_request
    
    Use a MockOdooHttpRequest class.
    
    This class supports 2 properties redirect and website that are
    added programatically by some odoo modules.
    
    Fix a bug with the session store. Previous code used the session store
    directly instead of creating a new session.
    
    Properly propagate the cursor, uid and context from the env
    to the request.
    
    yield the request object so that it can be used inside the test.
    This is required in some advanced test cases with website_sale related
    features.

--- test_http_request/common.py
+++ test_http_request/common.py
@@ -1,13 +1,47 @@
 # © 2019 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
+import werkzeug
 from contextlib import contextmanager
+from odoo.addons.http_routing.models.ir_http import url_for
 from odoo.api import Environment
-from odoo.http import HttpRequest, OpenERPSession
+from odoo.http import HttpRequest, OpenERPSession, _request_stack
 from odoo.tools import config
 from werkzeug.contrib.sessions import FilesystemSessionStore
 from werkzeug.test import EnvironBuilder
 from werkzeug.wrappers import Request
+
+
+class _MockOdooHttpRequest(HttpRequest):
+
+    @staticmethod
+    def redirect(url, code=302):
+        """Add the `redirect` method to the request.
+
+        This method is added programatically by the module http_routing:
+            odoo/addons/http_routing/models/ir_http.py (method _dispatch)
+        """
+        return werkzeug.utils.redirect(url_for(url), code)
+
+    @property
+    def website(self):
+        """Add the `website` property to the request.
+
+        The attribute is added programatically by the module website:
+            odoo/addons/website/models/ir_http.py (method _add_dispatch_parameters)
+
+        The attribute is added as a property so that the request object
+        can be used even if the module website is not installed.
+        """
+        return self.env['website'].get_current_website()
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        """Prevent commiting the transaction when exiting the HTTP request.
+
+        Since the request uses the same cursor as the test fixture,
+        the cursor must not be commited when exiting the request.
+        """
+        _request_stack.pop()
 
 
 @contextmanager
@@ -24,8 +58,9 @@ def mock_odoo_request(env: Environment, method='POST'):
     :param env: the odoo environment to bind with the request.
     :param method: the HTTP method called during the request.
     """
-    session = FilesystemSessionStore(
+    session_store = FilesystemSessionStore(
         config.session_dir, session_class=OpenERPSession, renew_missing=True)
+    session = session_store.new()
     session.db = env.cr.dbname
     session.uid = env.uid
     session.context = env.context
@@ -35,7 +70,11 @@ def mock_odoo_request(env: Environment, method='POST'):
     httprequest = Request(environ)
     httprequest.session = session
 
-    odoo_http_request = HttpRequest(httprequest)
+    odoo_http_request = _MockOdooHttpRequest(httprequest)
     odoo_http_request._env = env
+    odoo_http_request._cr = env.cr
+    odoo_http_request._uid = env.uid
+    odoo_http_request._context = env.context
+
     with odoo_http_request:
-        yield
+        yield odoo_http_request
