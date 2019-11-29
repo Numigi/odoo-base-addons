ff9eba977a650c640eca7d1b0692a5bd6a3b25c9
David Dufresne <27902736+ddufresne@users.noreply.github.com>
2019-11-29 14:24:11 -0500

    Add konvergo (#195)
    * Add konvergo
    
    * Add missing dependencies for mis_builder
    
    * Typo
    
    * Add python requirements for mis_builder
    
    * Typo
    
    * Remove xlsxwriter (already in odoo image)

---
+++ konvergo_base/__manifest__.py
@@ -0,0 +1,20 @@
+# © 2019 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    'name': 'Konvergo',
+    'version': '1.0.0',
+    'author': 'Numigi',
+    'maintainer': 'Numigi',
+    'license': 'LGPL-3',
+    'category': 'Other',
+    'summary': 'Dependencies for Konvergo',
+    'depends': [
+        # Numigi/odoo-base
+        'numipack',
+
+        # OCA/social
+        'mail_debrand',  # TA#16549
+    ],
+    'installable': True,
+}
