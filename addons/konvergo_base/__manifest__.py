3b0fc78dee24091f837feb4478befa181b5e4109
Jordi Riera <547282+foutoucour@users.noreply.github.com>
2020-02-06 10:20:51 -0500

    disable publisher cron job (TA#16530) (#218)
    * disable publisher cron job (TA#16530)
    
    * set the favicon and title as a module
    
    Co-authored-by: David Dufresne <27902736+ddufresne@users.noreply.github.com>

--- addons/konvergo_base/__manifest__.py
+++ addons/konvergo_base/__manifest__.py
@@ -1,4 +1,4 @@
-# © 2019 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# © 2020 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 {
@@ -12,11 +12,13 @@
     'depends': [
         # Numigi/odoo-base
         'numipack',
+        'numitech',
         # commented for now. It is not compatible with konvergo_login_page
         # see TA#17781
         # 'odoo-debrand',  # TA#16526
         'konvergo_favicon_title',  # TA#16527
         'konvergo_login_page',  # TA#18145
+        'konvergo_cron_publisher',  # TA#16530
 
         # OCA/social
         'mail_debrand',  # TA#16549
---
+++ addons/konvergo_cron_publisher/README.rst
@@ -0,0 +1,11 @@
+Konvergo Cron Publisher
+=======================
+Disable the ir_cron Publisher: Update Notification.
+
+Contributors
+------------
+* Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+
+More information
+----------------
+* Meet us at https://bit.ly/numigi-com
