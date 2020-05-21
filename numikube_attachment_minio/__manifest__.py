e80500e69ef7ec449aae9e202484b9f2e64f8493
David Dufresne <27902736+ddufresne@users.noreply.github.com>
2020-05-21 09:42:41 -0400

    TA#23899 Add numikube_attachment_minio (#266)
    * Add numikube_attachment_minio
    
    * Fix reference to model name
    
    * Add module multi_step_wizard
    
    This module is a new dependency of account_bank_statement_import_txt_xlsx.
    
    * Add missing OCA module

---
+++ numikube_attachment_minio/__manifest__.py
@@ -0,0 +1,16 @@
+# © 2020 - today Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    "name": "NumiKube Attachment Minio",
+    "version": "1.0.0",
+    "author": "Numigi",
+    "maintainer": "Numigi",
+    "license": "LGPL-3",
+    "category": "Other",
+    "summary": "Attachment Minio for the Numigi Kubernetes infrastructure",
+    "depends": ["attachment_minio",],
+    "data": ["data/ir_config_parameter.xml",],
+    "post_init_hook": "post_init_hook",
+    "installable": True,
+}
