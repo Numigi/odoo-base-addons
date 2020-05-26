fae1928e709e646c10b3f9dca1c5813ba0f46d6c
David Dufresne <27902736+ddufresne@users.noreply.github.com>
2020-05-25 21:22:25 -0400

    TA#23985 Add module numikube_database_backup (#267)
    * Add module numikube_database_backup
    
    * Auto create attachments bucket
    
    * Fix attachment_minio

--- numikube_attachment_minio/init_hook.py
+++ numikube_attachment_minio/init_hook.py
@@ -1,11 +1,33 @@
 # © 2020 - today Numigi (tm) and all its contributors (https://bit.ly/numigiens)
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
+from minio import Minio
 from odoo import SUPERUSER_ID
 from odoo.api import Environment
 
+BUCKET_NAME = "attachments"
+
 
 def post_init_hook(cr, _):
+    _auto_create_minio_bucket()
+    _transfer_existing_attachments_to_minio(cr)
+
+
+def _transfer_existing_attachments_to_minio(cr):
     with Environment.manage():
         env = Environment(cr, SUPERUSER_ID, {})
-        env["ir.attachment"].force_storage()
+        attachments = env["ir.attachment"].search([])
+        for attachment in attachments:
+            attachment.datas = attachment.datas
+
+
+def _auto_create_minio_bucket():
+    client = Minio(
+        "minio:9000",
+        access_key="minio",
+        secret_key="miniosecret",
+        secure=False,
+    )
+    bucket_names = {b.name for b in client.list_buckets()}
+    if BUCKET_NAME not in bucket_names:
+        client.make_bucket(BUCKET_NAME, location="us-east-1")
---
+++ numikube_database_backup/__manifest__.py
@@ -0,0 +1,15 @@
+# © 2020 - today Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    "name": "NumiKube Database Backup",
+    "version": "1.0.0",
+    "author": "Numigi",
+    "maintainer": "Numigi",
+    "license": "LGPL-3",
+    "category": "Other",
+    "summary": "Database Backups for the Numigi Kubernetes infrastructure",
+    "depends": ["base",],
+    "data": ["data/ir_cron.xml",],
+    "installable": True,
+}
