509c170c09f8e4d53bc8475328eafa0a42176f40
David Dufresne <27902736+ddufresne@users.noreply.github.com>
2020-07-16 17:33:36 -0400

    TA#25365 Add minio backups rotation (#289)
    * TA#25365 Add minio backups rotation

--- numikube_database_backup/tests/test_backups.py
+++ numikube_database_backup/tests/test_backups.py
@@ -1,22 +1,40 @@
 # © 2020 - today Numigi (tm) and all its contributors (https://bit.ly/numigiens)
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
+from datetime import datetime
 from odoo.tests import TransactionCase
 from odoo.addons.numikube_minio.minio import get_minio_client, get_bucket_names
 
 
 class TestBackups(TransactionCase):
-    def test_backup(self):
-        filenames_before = self._get_filenames()
+    def setUp(self):
+        super().setUp()
+        self.db_name = self.env.cr.dbname
 
-        self._execute_backup()
-        assert self._bucket_exists()
+    def test_hourly_backup(self):
+        self._execute_hourly_backup()
+        filenames = self._get_filenames()
+        backup_number = datetime.now().hour % 3
+        expected_filename = "{db_name}-hourly-{backup_number}.dump".format(
+            db_name=self.db_name, backup_number=backup_number
+        )
+        assert expected_filename in filenames
 
-        filenames_after = self._get_filenames()
-        assert len(filenames_after) == len(filenames_before) + 1
+    def test_daily_backup(self):
+        self._execute_daily_backup()
+        filenames = self._get_filenames()
+        backup_number = datetime.now().isoweekday()
+        expected_filename = "{db_name}-daily-{backup_number}.dump".format(
+            db_name=self.db_name, backup_number=backup_number
+        )
+        assert expected_filename in filenames
 
-    def _execute_backup(self):
+    def _execute_hourly_backup(self):
         cron = self.env.ref("numikube_database_backup.backup_cron")
+        cron.method_direct_trigger()
+
+    def _execute_daily_backup(self):
+        cron = self.env.ref("numikube_database_backup.daily_backup_cron")
         cron.method_direct_trigger()
 
     def _get_filenames(self):
