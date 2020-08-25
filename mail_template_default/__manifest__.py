feb3fa77f480d48175f98ce92361c963213fec28
cuong.nmtm <cuong.nmtm@komit-consulting.com>
2020-08-25 17:51:26 +0700

    Add translate and update summary

--- mail_template_default/__manifest__.py
+++ mail_template_default/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Mail Template Default",
-    "summary": "",
+    "summary": "Add ability to pre-define default mail template by object",
     "version": "12.0.1.0.0",
     "website": "https://bit.ly/numigi-com",
     "author": "Numigi",
