094bd93fa11eaec26316665e3a197b64f261f705
David Dufresne <27902736+ddufresne@users.noreply.github.com>
2020-08-28 14:28:51 -0400

    Remove attachment migration script (#305)
    The script is redundant with the one found in the module base_attachment_object_storage.
    Also the later one is more performant.

--- numikube_attachment_minio/init_hook.py
+++ numikube_attachment_minio/init_hook.py
@@ -3,22 +3,13 @@
 
 from odoo import SUPERUSER_ID
 from odoo.api import Environment
-from odoo.addons.numikube_minio.minio import get_minio_client, auto_create_bucket
+from odoo.addons.numikube_minio.minio import get_minio_client
 
 BUCKET_NAME = "attachments"
 
 
 def post_init_hook(cr, _):
     _auto_create_minio_bucket()
-    _transfer_existing_attachments_to_minio(cr)
-
-
-def _transfer_existing_attachments_to_minio(cr):
-    with Environment.manage():
-        env = Environment(cr, SUPERUSER_ID, {})
-        attachments = env["ir.attachment"].search([])
-        for attachment in attachments:
-            attachment.datas = attachment.datas
 
 
 def _auto_create_minio_bucket():
