7fe8fad4bbd70f58cc71dfba7943d99e8e276c71
David Dufresne <27902736+ddufresne@users.noreply.github.com>
2020-12-16 11:35:24 -0500

    TA#29490 add module prod2x_user_activation (#368)

---
+++ prod2x_user_activation/__manifest__.py
@@ -0,0 +1,17 @@
+# © 2020 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    "name": "Prod2x User Activation",
+    "version": "1.0.0",
+    "author": "Numigi",
+    "maintainer": "Numigi",
+    "license": "LGPL-3",
+    "category": "Other",
+    "summary": "Automatically activate users in staging environments",
+    "data": [
+        "security/res_groups.xml",
+    ],
+    "depends": ["prod2x",],
+    "installable": True,
+}
