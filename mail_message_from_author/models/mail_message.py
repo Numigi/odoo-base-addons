02b555a522d7f6d36b26bd3819426a5818a6c8ce
David Dufresne <27902736+ddufresne@users.noreply.github.com>
2021-01-22 08:15:55 -0500

    TA#30528 only propagate author when email is from odoobot (#64)
    * TA#30528 only propagate author when email is from odoobot
    
    Otherwise, the module breaks the behavior of email templates.

--- mail_message_from_author/models/mail_message.py
+++ mail_message_from_author/models/mail_message.py
@@ -8,25 +8,28 @@ class MailMessage(models.Model):
 
     _inherit = 'mail.message'
 
-    def _propagate_autor_to_email_from(self):
-        messages_with_author_emails = self.filtered(lambda m: m.author_id.email)
-        for message in messages_with_author_emails:
-            author = message.author_id
-            expected_email_from = "{} <{}>".format(author.name, author.email)
-            if expected_email_from != message.email_from:
-                message.email_from = expected_email_from
-
     @api.model
     def create(self, vals):
         message = super().create(vals)
-        message._propagate_autor_to_email_from()
+        message._propagate_author_to_email_from()
         return message
 
     @api.multi
     def write(self, vals):
         result = super().write(vals)
 
-        if vals.get('email_from') or vals.get('author_id'):
-            self._propagate_autor_to_email_from()
+        if vals.get('email_from') or vals.get('author_idgs'):
+            self._propagate_author_to_email_from()
 
         return result
+
+    def _propagate_author_to_email_from(self):
+        odoobot = self.env.ref("base.user_root").sudo()
+        messages_to_propagate = self.filtered(
+            lambda m: m.author_id.email and odoobot.email in m.email_from
+        )
+        for message in messages_to_propagate:
+            author = message.author_id
+            expected_email_from = "{} <{}>".format(author.name, author.email)
+            if expected_email_from != message.email_from:
+                message.email_from = expected_email_from
--- mail_message_from_author/tests/test_mail_message.py
+++ mail_message_from_author/tests/test_mail_message.py
@@ -5,30 +5,45 @@ from odoo.tests import common
 
 
 class TestMailMessage(common.SavepointCase):
-
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
-        cls.customer_name = "John Doe"
-        cls.customer_email = "john.doe@example.com"
-        cls.expected_email_from = "John Doe <john.doe@example.com>"
-        cls.customer = cls.env['res.partner'].create({
-            'name': cls.customer_name,
-            'email': cls.customer_email,
-        })
-        cls.product = cls.env['product.product'].create({'name': 'My Product'})
-        cls.order = cls.env['sale.order'].create({
-            'partner_id': cls.customer.id,
-            'order_line': [(0, 0, {
-                'name': '/',
-                'product_id': cls.product.id,
-                'product_uom_id': cls.product.uom_id.id,
-                'product_uom_qty': 1,
-            })]
-        })
+        cls.user_demo = cls.env.ref("base.user_demo")
+        cls.user_demo.groups_id |= cls.env.ref("sales_team.group_sale_manager")
 
-    def test_on_message_post__author_propagated_to_email_from(self):
-        message_body = "A message from my customer"
-        self.order.message_post(body=message_body, author_id=self.customer.id)
-        message = self.order.message_ids.filtered(lambda m: message_body in m.body)
-        assert message.email_from == self.expected_email_from
+        cls.author_email = "john.doe@example.com"
+        cls.customer = cls.env["res.partner"].create(
+            {"name": "My Customer"},
+        )
+        cls.author = cls.env["res.partner"].create(
+            {"name": "Message Author", "email": cls.author_email}
+        )
+        cls.product = cls.env["product.product"].create({"name": "My Product"})
+        cls.order = cls.env["sale.order"].create(
+            {
+                "partner_id": cls.customer.id,
+                "order_line": [
+                    (
+                        0,
+                        0,
+                        {
+                            "name": "/",
+                            "product_id": cls.product.id,
+                            "product_uom_id": cls.product.uom_id.id,
+                            "product_uom_qty": 1,
+                        },
+                    )
+                ],
+            }
+        )
+        cls.body = "A message from my customer"
+
+    def test_if_from_odoobot__author_propagated(self):
+        self.order.sudo().message_post(body=self.body, author_id=self.author.id)
+        message = self.order.message_ids.filtered(lambda m: self.body in m.body)
+        assert self.author_email in message.email_from
+
+    def test_if_not_from_odoobot__author_not_propagated(self):
+        self.order.sudo(self.user_demo).message_post(body=self.body, author_id=self.author.id)
+        message = self.order.message_ids.filtered(lambda m: self.body in m.body)
+        assert self.author_email not in message.email_from
