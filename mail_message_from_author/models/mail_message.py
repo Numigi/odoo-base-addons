eb2f4622fec7d791e3072c9daa84a6781e47891c
David Dufresne <27902736+ddufresne@users.noreply.github.com>
2021-01-22 08:07:51 -0500

    Prevent singleton error

--- mail_message_from_author/models/mail_message.py
+++ mail_message_from_author/models/mail_message.py
@@ -11,26 +11,24 @@ class MailMessage(models.Model):
     @api.model
     def create(self, vals):
         message = super().create(vals)
-        message._propagate_author_to_email_from_if_required()
+        message._propagate_author_to_email_from()
         return message
 
     @api.multi
     def write(self, vals):
         result = super().write(vals)
 
-        if vals.get('email_from') or vals.get('author_id'):
-            self._propagate_author_to_email_from_if_required()
+        if vals.get('email_from') or vals.get('author_idgs'):
+            self._propagate_author_to_email_from()
 
         return result
 
-    def _propagate_author_to_email_from_if_required(self):
-        odoobot = self.env.ref("base.user_root").sudo()
-        if odoobot.email in self.email_from:
-            self._propagate_author_to_email_from()
-
     def _propagate_author_to_email_from(self):
-        messages_with_author_emails = self.filtered(lambda m: m.author_id.email)
-        for message in messages_with_author_emails:
+        odoobot = self.env.ref("base.user_root").sudo()
+        messages_to_propagate = self.filtered(
+            lambda m: m.author_id.email and odoobot.email in m.email_from
+        )
+        for message in messages_to_propagate:
             author = message.author_id
             expected_email_from = "{} <{}>".format(author.name, author.email)
             if expected_email_from != message.email_from:
