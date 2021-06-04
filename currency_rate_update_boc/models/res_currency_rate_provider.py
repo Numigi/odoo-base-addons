3c4aa6c8644b986e80287e4e0c41181f0446ab05
Gabriel Gosselin Roberge <56849645+gabgosrob@users.noreply.github.com>
2021-06-04 15:17:27 -0400

    update res_currency_rate_provider logic

--- currency_rate_update_boc/models/res_currency_rate_provider.py
+++ currency_rate_update_boc/models/res_currency_rate_provider.py
@@ -20,76 +20,6 @@ class ResCurrencyRateProvider(models.Model):
             return super()._get_supported_currencies()
         return SUPPORTED_CURRENCIES_BOC
 
-    """@api.multi
-    def _obtain_rates(self, base_currency, currencies, date_from, date_to):
-        self.ensure_one()
-        if self.service != "bank_of_canada":
-            return super()._obtain_rates(base_currency, currencies, date_from, date_to)
-
-        observations = self._get_rates_from_boc(date_from, date_to)
-        result = defaultdict(dict)
-
-        for currency in currencies:
-            for date, rate in self._iter_rate_from_boc_observations(
-                observations, base_currency, currency
-            ):
-                result[date][currency] = rate
-
-        return result
-
-    def _get_rates_from_boc(self, date_from, date_to):
-        response = requests.request("GET", API_ADDRESS)
-        if response.status_code >= 400:
-            raise ValidationError(
-                _(
-                    "The request to the Valet api of the Bank of Canada terminated with an error.\n\n{}".format(
-                        response.text
-                    )
-                )
-            )
-
-        observations = response.json()["observations"]
-        date_from = date_from.strftime("%Y-%m-%d")
-        date_to = date_to.strftime("%Y-%m-%d")
-
-        return [o for o in observations if date_from <= o["d"] <= date_to]
-
-    def _iter_rate_from_boc_observations(self, observations, base_currency, currency):
-        if base_currency == "CAD":
-            yield from self._iter_cad2x(observations, currency)
-        elif currency == "CAD":
-            yield from self._iter_x2cad(observations, base_currency)
-        else:
-            yield from self._iter_x2x(observations, base_currency, currency)
-
-    def _iter_cad2x(self, observations, currency):
-        exchange = f"FX{currency}CAD"
-        for observation in observations:
-            if exchange in observation:
-                rate = float(observation[exchange]["v"])
-                yield observation["d"], round(rate, 4)
-
-    def _iter_x2cad(self, observations, currency):
-        exchange = f"FX{currency}CAD"
-        for observation in observations:
-            if exchange in observation:
-                rate = 1 / float(observation[exchange]["v"])
-                yield observation["d"], round(rate, 4)
-
-    def _iter_x2x(self, observations, base_currency, currency):
-        first_exchange = f"FX{base_currency}CAD"
-        second_exchange = f"FX{currency}CAD"
-        for observation in observations:
-            if first_exchange in observation and second_exchange in observation:
-                rate = (
-                    1
-                    / float(observation[first_exchange]["v"])
-                    * float(observation[second_exchange]["v"])
-                )
-                yield observation["d"], round(rate, 4)"""
-
-    # AUTRE SOLUTION
-
     @api.multi
     def _obtain_rates(self, base_currency, currencies, date_from, date_to):
         self.ensure_one()
@@ -98,7 +28,7 @@ class ResCurrencyRateProvider(models.Model):
 
         observations = self._get_rates_from_boc(
             base_currency, currencies, date_from, date_to
-        )
+        )["observations"]
         result = self._process_observations(observations, base_currency, currencies)
 
         return result
@@ -107,7 +37,7 @@ class ResCurrencyRateProvider(models.Model):
         url = self._get_boc_url(base_currency, currencies, date_from, date_to)
         response = self._get_boc_response(url)
 
-        return response
+        return response.json()
 
     def _get_boc_url(self, base_currency, currencies, date_from, date_to):
         date_from = date_from.strftime("%Y-%m-%d")
@@ -142,10 +72,11 @@ class ResCurrencyRateProvider(models.Model):
         result = defaultdict(dict)
         for observation in observations:
             for currency in currencies:
-                result[observation["d"]][
-                    currency
-                ] = self._get_rate_from_boc_observation(
-                    observation, base_currency, currency
+                result[observation["d"]][currency] = round(
+                    self._get_rate_from_boc_observation(
+                        observation, base_currency, currency
+                    ),
+                    4,
                 )
 
         return result
@@ -153,12 +84,12 @@ class ResCurrencyRateProvider(models.Model):
     def _get_rate_from_boc_observation(self, observation, base_currency, currency):
         if base_currency == "CAD" or currency == "CAD":
             series = f"FX{base_currency}{currency}"
-            return self._get_rate_from_boc_observation(observation, series)
+            return self._get_rate_from_boc_series(observation, series)
         else:
             first_series = f"FX{base_currency}CAD"
             second_series = f"FXCAD{currency}"
-            first_rate = self._get_rate_from_boc_observation(first_series)
-            second_rate = self._get_rate_from_boc_observation(second_series)
+            first_rate = self._get_rate_from_boc_series(observation, first_series)
+            second_rate = self._get_rate_from_boc_series(observation, second_series)
 
             return first_rate * second_rate
 
--- currency_rate_update_boc/tests/test_currency.py
+++ currency_rate_update_boc/tests/test_currency.py
@@ -9,7 +9,6 @@ from datetime import date, timedelta
 from odoo.tests.common import SavepointCase
 from ddt import ddt, data
 from odoo.exceptions import ValidationError
-from ..models.currency import BASE_API_ADDRESS
 
 
 @ddt
@@ -21,82 +20,57 @@ class TestBocRateProvider(SavepointCase):
         cls.cad = cls.env.ref("base.CAD")
         cls.cad.active = True
 
-        cls.date = date(2021, 5, 28)
+        cls.date = date(2021, 5, 3)
         cls.date_string = cls.date.strftime("%Y-%m-%d")
 
         cls.provider = cls.env["res.currency.rate.provider"].create(
             {"service": "bank_of_canada", "currency_ids": [(4, cls.cad.id)]}
         )
 
+        cls.test_boc_url = "https://www.bankofcanada.ca/valet/observations/FXUSDCAD,FXEURCAD?start_date=2021-05-03&end_date=2021-05-03"
+
     @data("CAD", "USD", "EUR")
     def test_supported_currencies(self, currency_code):
         supported_currencies = self.provider.available_currency_ids.mapped("name")
         assert currency_code in supported_currencies
 
-    def test_get_rates(self):
+    def test_boc_response(self):
         json_data = self._get_rates_json()
         with requests_mock.Mocker() as m:
-            m.get(BASE_API_ADDRESS, json=json_data)
-            rates = self.provider._get_rates_from_boc(self.date, self.date)
-            assert rates == json_data["observations"]
+            m.get(self.test_boc_url, json=json_data)
+            rates = self.provider._get_boc_response(self.test_boc_url).json()
+            assert rates == json_data
 
     @data(401, 404, 410, 500, 502)
-    def test_get_rates_exceptions(self, error_code):
+    def test_boc_response_exceptions(self, error_code):
         with requests_mock.Mocker() as m:
-            m.get(BASE_API_ADDRESS, status_code=error_code)
+            m.get(self.test_boc_url, status_code=error_code)
             with pytest.raises(ValidationError):
-                self.provider._get_rates_from_boc(self.date, self.date)
+                self.provider._get_boc_response(self.test_boc_url)
 
-    def test_rates_filtered_cad2x(self):
-        json_data = self._get_rates_json()
-        with requests_mock.Mocker() as m:
-            m.get(BASE_API_ADDRESS, status_code=200, json=json_data)
-            filtered_rates = self.provider._obtain_rates(
-                "CAD", ["USD", "EUR"], self.date, self.date
-            )
-            assert filtered_rates[self.date_string]["USD"] == 1.2086
-            assert filtered_rates[self.date_string]["EUR"] == 1.4723
+    def test_rates_cad2x(self):
+        rates = self.provider._obtain_rates("CAD", ["USD", "EUR"], self.date, self.date)
+        assert rates[self.date_string]["USD"] == round(1 / 1.2279, 4)
+        assert rates[self.date_string]["EUR"] == round(1 / 1.4808, 4)
 
-    def test_rates_filtered_x2cad(self):
-        json_data = self._get_rates_json()
-        with requests_mock.Mocker() as m:
-            m.get(BASE_API_ADDRESS, status_code=200, json=json_data)
-            filtered_rates = self.provider._obtain_rates(
-                "USD", ["CAD"], self.date, self.date
-            )
-            assert filtered_rates[self.date_string]["CAD"] == round(1 / 1.2086, 4)
+    def test_rates_x2cad(self):
+        rates = self.provider._obtain_rates("USD", ["CAD"], self.date, self.date)
+        assert rates[self.date_string]["CAD"] == 1.2279
 
-    def test_rates_filtered_x2x(self):
-        json_data = self._get_rates_json()
-        with requests_mock.Mocker() as m:
-            m.get(BASE_API_ADDRESS, status_code=200, json=json_data)
-            filtered_rates = self.provider._obtain_rates(
-                "USD", ["EUR"], self.date, self.date
-            )
-            assert filtered_rates[self.date_string]["EUR"] == round(
-                1 / 1.2086 * 1.4723, 4
-            )
+    def test_rates_x2x(self):
+        rates = self.provider._obtain_rates("USD", ["EUR"], self.date, self.date)
+        assert rates[self.date_string]["EUR"] == round(1.2279 * 1 / 1.4808, 4)
 
     def test_invalid_currency(self):
-        json_data = self._get_rates_json()
-        with requests_mock.Mocker() as m:
-            m.get(BASE_API_ADDRESS, status_code=200, json=json_data)
-            filtered_rates = self.provider._obtain_rates(
-                "CAD", ["ZZZZ"], self.date, self.date
-            )
-            assert not filtered_rates
+        with pytest.raises(ValidationError):
+            rates = self.provider._obtain_rates("USD", ["ZZZZ"], self.date, self.date)
 
     def test_invalid_date(self):
-        json_data = self._get_rates_json()
-        with requests_mock.Mocker() as m:
-            m.get(BASE_API_ADDRESS, status_code=200, json=json_data)
-            filtered_rates = self.provider._obtain_rates(
-                "CAD", ["USD", "EUR"], self.date, self.date - timedelta(5)
-            )
-            assert not filtered_rates
+        with pytest.raises(ValidationError):
+            rates = self.provider._obtain_rates("USD", ["ZZZZ"], self.date, self.date - timedelta(5))
 
     def _get_rates_json(self):
         dir_path = os.path.dirname(os.path.realpath(__file__))
-        with open(f"{dir_path}/FX_RATES_DAILY.json") as f:
+        with open(f"{dir_path}/FX_RATES.json") as f:
             data = json.load(f)
             return data
