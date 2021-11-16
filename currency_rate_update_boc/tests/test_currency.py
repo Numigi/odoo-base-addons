943452a598541c089a14f6befddebffec049a326
David Dufresne <27902736+ddufresne@users.noreply.github.com>
2021-11-16 11:32:20 -0500

    TA#40589 load the rates of the previous day

--- currency_rate_update_boc/tests/test_currency.py
+++ currency_rate_update_boc/tests/test_currency.py
@@ -5,6 +5,7 @@ import requests_mock
 import json
 import os
 import pytest
+from contextlib import contextmanager
 from datetime import date, timedelta
 from odoo.tests.common import SavepointCase
 from ddt import ddt, data
@@ -17,6 +18,8 @@ class TestBocRateProvider(SavepointCase):
     def setUpClass(cls):
         super().setUpClass()
 
+        cls.company = cls.env.user.company_id
+
         cls.cad = cls.env.ref("base.CAD")
         cls.cad.active = True
 
@@ -27,7 +30,7 @@ class TestBocRateProvider(SavepointCase):
             {"service": "bank_of_canada", "currency_ids": [(4, cls.cad.id)]}
         )
 
-        cls.test_boc_url = "https://www.bankofcanada.ca/valet/observations/FXUSDCAD,FXEURCAD?start_date=2021-05-03&end_date=2021-05-03"
+        cls.test_boc_url = "https://www.bankofcanada.ca/valet/observations/"
 
     @data("CAD", "USD", "EUR")
     def test_supported_currencies(self, currency_code):
@@ -35,9 +38,7 @@ class TestBocRateProvider(SavepointCase):
         assert currency_code in supported_currencies
 
     def test_boc_response(self):
-        json_data = self._get_rates_json()
-        with requests_mock.Mocker() as m:
-            m.get(self.test_boc_url, json=json_data)
+        with self._mock_boc_response(["FXUSDCAD"]):
             rates = self.provider._get_boc_response(self.test_boc_url).json()
             assert rates == json_data
 
@@ -68,6 +69,30 @@ class TestBocRateProvider(SavepointCase):
     def test_invalid_date(self):
         with pytest.raises(ValidationError):
             rates = self.provider._obtain_rates("USD", ["ZZZZ"], self.date, self.date - timedelta(5))
+
+    def test_cron__loads_rates_one_day_prior(self):
+        """Test that the rates of yesterday are loaded by the cron.
+
+        With the Bank of Canada, the daily rates are only available the day after.
+        When the cron is executed the rates of the current date are not available.
+
+        The rates of the current date are loaded the following day.
+        Otherwise, the cron ends up loading no rate at all.
+        """
+        self.provider.last_successful_run = self.date
+        self.provider.next_run = self.date + timedelta(1)
+        with self._mock_boc_response(["FXUSDCAD"]):
+            self.env["res.currency.rate.provider"]._scheduled_update()
+        rate = self.cad._get_rates(self.company, self.date)[self.cad.id]
+        assert round(rate, 4) == 1.2279
+
+    @contextmanager
+    def _mock_boc_response(self, series):
+        json_data = self._get_rates_json()
+        with requests_mock.Mocker() as m:
+            url = self.test_boc_url + ",".join(series)
+            m.get(url, json=json_data)
+            yield
 
     def _get_rates_json(self):
         dir_path = os.path.dirname(os.path.realpath(__file__))
