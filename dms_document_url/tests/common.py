b01a005caa90d913c6bace57f1548bab6ee782d5
Majda EL MARIOULI <majdaelmariouli@gmail.com>
2022-09-26 13:39:35 +0100

    TA#46953 DMS Document URL (#103)

--- dms_document_url/tests/common.py
+++ dms_document_url/tests/common.py
@@ -2,8 +2,7 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 import uuid
-
-from odoo.addons.dms.tests import DocumentsBaseCase
+from odoo.addons.dms.tests.common import DocumentsBaseCase
 
 
 class DocumentsURLBaseCase(DocumentsBaseCase):
