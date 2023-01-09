1133229a6d1474d962a0577c30fd482ad05cf737
Abdellatif Benzbiria <a.benzbiria@gmail.com>
2023-01-09 12:57:05 +0100

    [MIG] base_extended_security: Migration to 14.0

--- base_extended_security/__manifest__.py
+++ base_extended_security/__manifest__.py
@@ -1,9 +1,9 @@
-# © 2019 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# © 2023 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 {
     'name': 'Base Extended Security',
-    'version': '1.1.1',
+    'version': "14.0.1.0.0",
     'author': 'Numigi',
     'maintainer': 'Numigi',
     'license': 'LGPL-3',
