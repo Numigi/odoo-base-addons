259cb20134903b303cc350de7c05099835777d53
Majda EL MARIOULI <majdaelmariouli@gmail.com>
2023-01-23 12:51:24 -0500

    TA#49755 Fix admin_light_user (#651)

--- admin_light_user/mask_admin_groups.py
+++ admin_light_user/mask_admin_groups.py
@@ -67,7 +67,9 @@ def _remove_admin_application_selection_fields(tree: etree._Element, env: api.En
         ]
         selected_groups = env['res.groups'].browse(selected_group_ids)
         if selected_groups[0].category_id in admin_applications:
-            node.getparent().remove(node)
+            if node in node.getparent():
+                print(selected_groups.mapped('name'))
+                node.getparent().remove(node)
 
 
 def _remove_admin_application_checkbox_fields(tree: etree._Element, env: api.Environment):
@@ -83,7 +85,9 @@ def _remove_admin_application_checkbox_fields(tree: etree._Element, env: api.Env
         selected_group_id = int(node.attrib['name'].replace('in_group_', ''))
         selected_group = env['res.groups'].browse(selected_group_id)
         if selected_group.category_id in admin_applications:
-            node.getparent().remove(node)
+            if node in node.getparent():
+                print(selected_group.mapped('name'))
+                node.getparent().remove(node)
 
 
 def _remove_separators_with_no_fields_below(tree: etree._Element):
