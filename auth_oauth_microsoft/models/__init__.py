bbc2c4335e8f77f11db64e364fd8f6aa9291175e
majouda <majdaelmariouli@gmail.com>
2023-02-09 18:14:56 -0500

    TA#48105 [ADD] module auth_oauth_microsoft

--- auth_oauth_microsoft/models/__init__.py
+++ auth_oauth_microsoft/models/__init__.py
@@ -1,4 +1,5 @@
-# © 2021 - today Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# © 2023 - Numigi (tm) and all its contributors (https://bit.ly/numigiens)
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 from . import res_users
+from . import auth_oauth
