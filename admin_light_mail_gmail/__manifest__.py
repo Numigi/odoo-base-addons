2e33e2edbd1642154e988ab182388846450cbd23
Lanto R <124650562+lanto-razafindrabe@users.noreply.github.com>
2023-09-11 18:52:07 +0300

    TA#56200 [ADD] admin_light_mail_outlook, admin_light_mail_gmail (#821)

---
+++ admin_light_mail_gmail/__manifest__.py
@@ -0,0 +1,17 @@
+# © 2023 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Admin Light Mail Gmail",
+    "version": "1.0.0",
+    "author": "Numigi",
+    "maintainer": "Numigi",
+    "website": "https://bit.ly/numigi-com",
+    "license": "AGPL-3",
+    'category': 'Other',
+    "depends": ["admin_light_mail", "fetchmail_gmail"],
+    "summary": """Extends Administration Light/Mail Servers rights to handle 
+    fetchmail_gmail functionalities.""",
+    "data": [],
+    "installable": True,
+}
