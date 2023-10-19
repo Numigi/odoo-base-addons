abc6d941a24a5e654daa441c3260d0ba8ce1974f
Lanto R <124650562+lanto-razafindrabe@users.noreply.github.com>
2023-10-19 17:34:05 +0300

    TA#57414 [i18N] event_allowed_ceu : translations (#125)
    * TA#57414 [i18N] event_allowed_ceu : translations
    
    * [UPD] module version

--- event_allowed_ceu/__manifest__.py
+++ event_allowed_ceu/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Event Allowed CEU",
-    "version": "1.0.0",
+    "version": "1.0.1",
     "author": "Numigi",
     "maintainer": "Numigi",
     "website": "https://bit.ly/numigi-com",
