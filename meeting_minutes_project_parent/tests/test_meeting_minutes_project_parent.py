486725123fb9de9808d6a1a5adc2c4ebd4e84958
Lanto Razafindrabe <lanto.razafindrabe@numigi.com>
2023-11-14 13:32:13 +0300

    TA#56215 [FIX] meeting_minutes_project_parent : unit test

--- meeting_minutes_project_parent/tests/test_meeting_minutes_project_parent.py
+++ meeting_minutes_project_parent/tests/test_meeting_minutes_project_parent.py
@@ -2,19 +2,32 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 
-from odoo.addons.meeting_minutes_project.tests.test_project_meeting_minutes import (
-    TestMeetingMinutesProject,
-)
+from odoo.tests.common import SavepointCase
 
 
-class TestMeetingMinutesProjectParent(TestMeetingMinutesProject):
+class TestMeetingMinutesProjectParent(SavepointCase):
     @classmethod
     def setUpClass(cls):
-        super(TestMeetingMinutesProjectParent, cls).setUpClass()
-        cls.project_3 = cls.env["project.project"].create({"name": "Project 3"})
+        super().setUpClass()
+        cls.project_1 = cls.env["project.project"].create({"name": "Project 1"})
+        cls.project_2 = cls.env["project.project"].create({"name": "Project 2"})
+
+        cls.task_1 = cls.env["project.task"].create(
+            {
+                "project_id": cls.project_1.id,
+                "name": "Task 1",
+            }
+        )
+
+        cls.task_2 = cls.env["project.task"].create(
+            {
+                "project_id": cls.project_1.id,
+                "name": "Task 2",
+            }
+        )
         cls.task_3 = cls.env["project.task"].create(
             {
-                "project_id": cls.project_3.id,
+                "project_id": cls.project_2.id,
                 "name": "Task 3",
             }
         )
@@ -22,9 +35,9 @@ class TestMeetingMinutesProjectParent(TestMeetingMinutesProject):
     def test_project_child_meeting_minutes(self):
         self.task_1.open_meeting_minutes()
         self.task_2.open_meeting_minutes()
-        self.project_1.write({"parent_id": self.project_3.id})
-        self.project_3._compute_meeting_minutes_project()
-        assert 2 == self.project_3.meeting_minutes_project_count
+        self.project_1.write({"parent_id": self.project_2.id})
+        self.project_2._compute_meeting_minutes_project()
+        assert 2 == self.project_2.meeting_minutes_project_count
         self.task_3.open_meeting_minutes()
-        self.project_3._compute_meeting_minutes_project()
-        assert 3 == self.project_3.meeting_minutes_project_count
+        self.project_2._compute_meeting_minutes_project()
+        assert 3 == self.project_2.meeting_minutes_project_count
