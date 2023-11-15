9101704e1d233c63fd4964d05641b5e874e5a799
Lanto R <124650562+lanto-razafindrabe@users.noreply.github.com>
2023-11-16 00:56:21 +0300

    TA#56213 [ADD] meeting_minutes : new base module (#126)
    * TA#56213 [ADD] meeting_minutes : new module

---
+++ meeting_minutes/__manifest__.py
@@ -0,0 +1,19 @@
+# © 2023 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Meeting Minutes",
+    "version": "1.0.0",
+    "author": "Numigi",
+    "maintainer": "Numigi",
+    "website": "https://bit.ly/numigi-com",
+    "license": "AGPL-3",
+    "category": "Other",
+    "depends": ["base"],
+    "summary": "Add an abstract model, which can be used on several objects in Odoo.",
+    "data": [
+        "security/ir.model.access.csv",
+        "views/meeting_minutes_mixin_views.xml",
+    ],
+    "installable": True,
+}
