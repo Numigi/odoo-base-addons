7384cbc33ae6f20e8564e3b6c5c70ebb73421ecf
Abdellatif BENZBIRIA <a.benzbiria@gmail.com>
2024-06-12 22:53:27 +0200

    TA#65405 [UPD][16.0] odoo-base: genaral settings (#935)
    TA#65405 [UPD][16.0] odoo-base: genaral settings
    
    ---------
    
    Co-authored-by: majouda <majdaelmariouli@gmail.com>

--- admin_light_base/__manifest__.py
+++ admin_light_base/__manifest__.py
@@ -2,6 +2,7 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 {
+<<<<<<< HEAD
     "name": "Admin Light",
     "version": "16.0.1.0.0",
     "author": "Numigi",
@@ -14,6 +15,21 @@
         "views/base.xml",
         "views/sequence.xml",
         "security/ir.model.access.csv",
+=======
+    'name': 'Admin Light',
+    'version': '1.0.2',
+    'author': 'Numigi',
+    'maintainer': 'Numigi',
+    'website': 'https://github.com/Numigi/odoo-base',
+    'license': 'LGPL-3',
+    'category': 'Other',
+    'summary': 'Add an admin menu with restricted functionalities.',
+    'depends': ['base'],
+    'data': [
+        'data/base.xml',
+        'data/sequence.xml',
+        'security/ir.model.access.csv',
+>>>>>>> f37ee2d (TA#65405 [UPD][16.0] odoo-base: genaral settings (#935))
     ],
     "installable": True,
     "application": True,
