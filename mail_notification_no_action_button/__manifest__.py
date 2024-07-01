69740e2a68b8b4119d1bbc1b81396d10964fa0a4
Abdellatif Benzbiria <a.benzbiria@gmail.com>
2024-07-01 17:31:15 +0100

    [16.0][MIG] : mail_notification_no_action_button

--- mail_notification_no_action_button/__manifest__.py
+++ mail_notification_no_action_button/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     'name': 'Mail Notification No Action Button',
-    'version': '1.0.0',
+    'version': '16.0.1.0.0',
     'author': 'Numigi',
     'maintainer': 'Numigi',
     'license': 'LGPL-3',
