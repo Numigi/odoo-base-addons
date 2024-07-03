232440850c88a6f839258e35c4998eb08e2a5203
Lanto R <124650562+lanto-razafindrabe@users.noreply.github.com>
2024-07-03 22:35:34 +0300

    TA#66793 [16.0][MIG] web_base_url_freeze (#960)
    Co-authored-by: Majda EL MARIOULI <majdaelmariouli@gmail.com>

---
+++ web_base_url_freeze/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2024-today Numigi and all its contributors (https://bit.ly/numigiens)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    "name": "Web Base Url Freeze",
+    "version": "16.0.1.0.0",
+    "author": "Numigi",
+    "maintainer": "Numigi",
+    "license": "LGPL-3",
+    "category": "Extra Tools",
+    "summary": "Automatically set the config parameter web.base.url.freeze.",
+    "depends": [
+        "base",
+    ],
+    "data": [
+        "data/ir_config_parameter.xml",
+    ],
+    "installable": True,
+}
