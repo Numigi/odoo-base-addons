1b8f40b1bf5a7185df19df0f9fa5d86ef637b6f4
Lanto R <124650562+lanto-razafindrabe@users.noreply.github.com>
2024-07-05 00:46:11 +0300

    TA#66782 [16.0][MIG] attachment_minio (#152)
    Co-authored-by: Abdellatif Benzbiria <a.benzbiria@gmail.com>

---
+++ attachment_minio/__init__.py
@@ -0,0 +1,6 @@
+# Copyright 2016 Camptocamp SA
+# Copyright 2020 Hibou Corp.
+# Copyright 2024-today Numigi and all its contributors (https://bit.ly/numigiens)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+
+from . import models
