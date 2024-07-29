5a6c01de50f91a8061ef487efda05021966d16cf
majouda <majdaelmariouli@gmail.com>
2024-07-29 17:45:49 -0400

    TA#66760 [16.0][MIG] admin_light_base

--- admin_light_base/__manifest__.py
+++ admin_light_base/__manifest__.py
@@ -1,20 +1,20 @@
-# © 2018 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# Copyright 2018 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 {
-    'name': 'Admin Light',
-    'version': '1.0.2',
-    'author': 'Numigi',
-    'maintainer': 'Numigi',
-    'license': 'LGPL-3',
-    'category': 'Other',
-    'summary': 'Add an admin menu with restricted functionalities.',
-    'depends': ['base'],
-    'data': [
-        'base.xml',
-        'sequence.xml',
-        'security/ir.model.access.csv',
+    "name": "Admin Light",
+    "version": "16.0.1.0.0",
+    "author": "Numigi",
+    "maintainer": "Numigi",
+    "license": "LGPL-3",
+    "category": "Other",
+    "summary": "Add an admin menu with restricted functionalities.",
+    "depends": ["base"],
+    "data": [
+        "views/base.xml",
+        "views/sequence.xml",
+        "security/ir.model.access.csv",
     ],
-    'installable': True,
-    'application': True
+    "installable": True,
+    "application": True,
 }
