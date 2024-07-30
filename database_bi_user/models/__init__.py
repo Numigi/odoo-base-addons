3344f0039f0b6f0a412884f39c2c27419acead18
majouda <majdaelmariouli@gmail.com>
2024-07-29 22:04:27 -0400

    TA#66762 [16.0][MIG] admin_light_calendar

--- database_bi_user/models/__init__.py
+++
@@ -1,8 +0,0 @@
-# © 2019 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
-# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
-
-from . import (
-    database_bi_user_update,
-    ir_private_field,
-    res_config_settings,
-)
