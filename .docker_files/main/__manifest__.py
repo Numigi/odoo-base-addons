a9efd1bec74b90d970320a8b2805742e95e77c71
majouda <majdaelmariouli@gmail.com>
2024-08-21 10:17:18 -0400

    TA#66805 [REM] base_external_report_layout: Remove Module

--- .docker_files/main/__manifest__.py
+++ .docker_files/main/__manifest__.py
@@ -15,7 +15,6 @@
         "admin_light_base",
         "admin_light_calendar",
         "attachment_minio",
-        "base_external_report_layout",
         "ir_attachment_access_token_portal",
         "lang_fr_activated",
         "mail_template_default",
