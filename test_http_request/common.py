acfc8a011ef3e36b29d36f9ea561d50f7ed00f5f
Abdellatif Benzbiria <a.benzbiria@gmail.com>
2024-08-27 18:32:47 +0100

    [16.0][MIG] test_http_request

--- test_http_request/common.py
+++ test_http_request/common.py
@@ -7,7 +7,7 @@ from contextlib import contextmanager
 from io import BytesIO
 from odoo.addons.http_routing.models.ir_http import url_for
 from odoo.api import Environment
-from odoo.http import HTTPRequest, JsonRPCDispatcher, _request_stack ,Session
+from odoo.http import HTTPRequest, JsonRPCDispatcher, _request_stack ,Session, Request
 from odoo.tools import config
 from typing import Optional, Union
 from odoo.http import FilesystemSessionStore
@@ -15,7 +15,7 @@ from odoo.http import FilesystemSessionStore
 from werkzeug.datastructures import ImmutableOrderedMultiDict
 from werkzeug.test import EnvironBuilder
 from werkzeug.urls import url_encode
-from werkzeug.wrappers import Request
+#from werkzeug.wrappers import Request
 
 
 class _MockOdooRequestMixin:
