740f9406ca9ffb84d45e4d78561661388e8eca64
Rivo Lalaina <rivo2302@gmail.com>
2024-09-18 15:18:06 +0300

    TA#66770 [MIG][16.0] admin_light_mail_gmail

--- .docker_files/main/__manifest__.py
+++ .docker_files/main/__manifest__.py
@@ -20,6 +20,7 @@
         "admin_light_company",
         "admin_light_gamification",
         "admin_light_mail",
+        "admin_light_mail_gmail",
         "attachment_minio",
         "lang_fr_activated",
         "mail_notification_no_action_button",
---
+++ admin_light_mail_gmail/__manifest__.py
@@ -0,0 +1,17 @@
+# © 2024 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Admin Light Mail Gmail",
+    "version": "16.0.1.0.0",
+    "author": "Numigi",
+    "maintainer": "Numigi",
+    "website": "https://bit.ly/numigi-com",
+    "license": "AGPL-3",
+    'category': 'Other',
+    "depends": ["admin_light_mail", "google_gmail"],
+    "summary": """Extends Administration Light/Mail Servers rights to handle
+    google_gmail functionalities.""",
+    "data": [],
+    "installable": True,
+}
