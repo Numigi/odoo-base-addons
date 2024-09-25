93a8989229c0f5dbc7b13dee2641008464d9e10e
Rivo Lalaina <rivo2302@gmail.com>
2024-09-25 09:51:01 +0300

    TA#66776 [MIG][16.0] base_extended_security

--- base_extended_security/models/ir_ui_view.py
+++ base_extended_security/models/ir_ui_view.py
@@ -1,3 +1,6 @@
+# Copyright 2024-today Numigi and all its contributors (https://bit.ly/numigiens)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
 from lxml import etree
 from odoo import api, models
 
