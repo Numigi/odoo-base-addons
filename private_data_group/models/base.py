6931bf30de06334a3aa9bc9f5a334afd6477da2e
Abdellatif Benzbiria <a.benzbiria@gmail.com>
2024-10-08 18:44:25 +0100

    [CLEAN] code cleaning

--- private_data_group/models/base.py
+++ private_data_group/models/base.py
@@ -6,50 +6,6 @@ from typing import Iterable
 from ..controllers.common import check_model_fields_access, extract_fields_from_domain
 
 
-METHODS_WITH_ORDERBY = [
-    'search',
-    'search_read',
-    'read_group',
-]
-
-
-ORDERBY_ARGUMENT_NAMES = {
-    'search': 'order',
-    'search_read': 'order',
-    'read_group': 'orderby',
-}
-
-
-ORDERBY_ARGUMENT_INDEXES = {
-    'search': 3,
-    'search_read': 4,
-    'read_group': 5,
-}
-
-
-def _get_orderby_from_args_and_kwargs(method: str, args: list, kwargs: dict):
-    """Get the orderby from the given args and kwargs.
-
-    If neither the args or kwargs contain the orderby (which is a valid case),
-    an empty orderby is returned.
-
-    :param method: the method called.
-    :param args: the arguments passed through rpc.
-    :param kargs: the keyword arguments passed through rpc.
-    :return: the given orderby.
-    """
-    argument_name = ORDERBY_ARGUMENT_NAMES[method]
-    argument_index = ORDERBY_ARGUMENT_INDEXES[method]
-
-    args = args or []
-    kwargs = kwargs or {}
-
-    if len(args) > argument_index:
-        return args[argument_index]
-
-    return kwargs.get(argument_name) or None
-
-
 def _parse_fields_from_orderby_clause(order_clause: str):
     parts = order_clause.split(',')
     return [p.split(' ')[0] for p in parts]
@@ -67,12 +23,6 @@ def _filter_unauthorized_fields(model: str, record_values: Iterable[dict]):
         return {k: v for k, v in r.items() if k not in private_fields}
 
     return [without_private_fields(r) for r in record_values]
-
-
-def _extract_groupby_from_args_and_kwargs(args: list, kwargs: dict):
-    groupby = args[2] if len(args) > 2 else kwargs.get('groupby', [])
-    groupby_fields = [groupby] if isinstance(groupby, str) else groupby
-    return [f.split(':')[0] for f in groupby_fields]
 
 
 class BasePrivateDataGroup(models.AbstractModel):
