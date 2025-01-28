124be9c8d495dcb10955361ae2f4894c814aabd5
Rivo Lalaina <rivo2302@gmail.com>
2025-01-28 16:34:22 +0300

    [14.0] [ADD] auditlog_binding_queue_job

---
+++ auditlog_binding_queue_job/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2024 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    'name': 'Auditlog Binding Queue Job',
+    'version': '14.0.1.0.0',
+    'author': 'Numigi',
+    'maintainer': 'Numigi',
+    'license': 'LGPL-3',
+    'category': 'Other',
+    'summary': 'Hide logs in the auditlog module if the requests come from a queue job.',
+    'depends': [
+        'auditlog',
+        'queue_job',
+    ],
+    'installable': True,
+}
