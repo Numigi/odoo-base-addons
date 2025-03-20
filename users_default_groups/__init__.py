a4d79b9db6018522edb3e909f403e92b98c62a56
Lanto R. <124650562+lanto-razafindrabe@users.noreply.github.com>
2025-03-20 09:10:16 +0300

    [IMP] Code quality

--- users_default_groups/__init__.py
+++ users_default_groups/__init__.py
@@ -1,10 +1,9 @@
-# Copyright 2024 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# Copyright 2025 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
-from . import models
 from odoo import api, SUPERUSER_ID
 
 
 def _defaut_config_param(cr, registry):
     env = api.Environment(cr, SUPERUSER_ID, {})
-    env['ir.config_parameter'].set_param('base_setup.default_user_rights', '1')
+    env["ir.config_parameter"].set_param("base_setup.default_user_rights", "1")
--- users_default_groups/__manifest__.py
+++ users_default_groups/__manifest__.py
@@ -1,16 +1,16 @@
-# Copyright 2024 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# Copyright 2025 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 {
     "name": "Users Default Groups",
     "summary": "Fix default groups settings",
-    "version": "1.0.0",
+    "version": "14.0.1.0.0",
     "category": "Base",
     "author": "Numigi",
     "license": "AGPL-3",
     "depends": ["base_setup"],
     "data": [],
     "installable": True,
-    'post_init_hook': '_defaut_config_param',
+    "post_init_hook": "_defaut_config_param",
     "auto_install": True,
 }
