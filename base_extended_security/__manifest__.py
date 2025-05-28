20b6424e9baebfcde822ea97dcffc25895a86849
Rivo Lalaina <rivo2302@gmail.com>
2025-05-28 17:48:48 +0300

    [16.0] [FIX] base_extended_security :  remove some logic

--- base_extended_security/__manifest__.py
+++ base_extended_security/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Base Extended Security",
-    "version": "16.0.1.0.1",
+    "version": "16.0.1.1.0",
     "author": "Numigi",
     "maintainer": "Numigi",
     "license": "LGPL-3",
