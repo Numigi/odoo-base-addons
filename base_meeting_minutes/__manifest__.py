a8c5be64e98f8f423a6185cc2df6b9acb61c82fa
Abdellatif BENZBIRIA <a.benzbiria@gmail.com>
2025-10-29 12:13:03 +0100

    Update __manifest__.py

--- base_meeting_minutes/__manifest__.py
+++ base_meeting_minutes/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Base Meeting Minutes",
-    "version": "12.0.1.0.0",
+    "version": "12.0.1.1.0",
     "author": "Numigi",
     "maintainer": "Numigi",
     "website": "https://bit.ly/numigi-com",
