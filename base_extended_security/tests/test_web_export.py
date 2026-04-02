cc4bd10880c9fed36d88b95b22fc3671fd4bd28b
Abdellatif Benzbiria <odoo-base@numigi.com>
2026-04-02 13:13:53 +0000

    [IMP] base_extended_security : Tu web export

--- base_extended_security/tests/test_web_export.py
+++ base_extended_security/tests/test_web_export.py
@@ -8,6 +8,13 @@ from .common import ControllerCase, mock_request_env
 from ..controllers.web_export import CSVControllerWithSecurity
 
 
+class DummyResponse:
+    """ Faux objet de réponse pour capturer les données renvoyées par make_response """
+
+    def __init__(self, data):
+        self.data = data
+
+
 class TestWebExport(ControllerCase):
 
     def setUp(self):
@@ -24,9 +31,16 @@ class TestWebExport(ControllerCase):
         }
         data_ = json.dumps(params)
 
-        with mock_request_env(self.env):
+        with mock_request_env(self.env) as mock_req:
+            # On configure le mock pour qu'il capture les données CSV générées
+            mock_req.make_response.side_effect = lambda data, *args, **kwargs: DummyResponse(data)
+
             response = self.controller.base(data_)
-            return response.data.decode("utf-8")
+
+            # Selon la version d'Odoo, le CSV peut être en bytes ou en string
+            if isinstance(response.data, bytes):
+                return response.data.decode("utf-8")
+            return str(response.data)
 
     def test_if_given_domain__domain_filter_applied_to_data(self):
         data_ = self._export(ids=[], domain=[])
