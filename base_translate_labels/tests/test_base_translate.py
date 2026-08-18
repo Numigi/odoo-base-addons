82112527c1342f0702813f16ffeb3849782fadf4
Abdellatif Benzbiria <odoo-base@numigi.com>
2026-08-18 15:34:19 +0100

    [18.0] base_translate_lables : TU

--- base_translate_labels/tests/test_base_translate.py
+++ base_translate_labels/tests/test_base_translate.py
@@ -10,7 +10,7 @@ class TestTranslationFrCa(TransactionCase):
     def setUpClass(cls):
         super().setUpClass()
         cls.term_model = cls.env["base.translate"]
-        cls.partner_model = cls.env["res.partner"]
+        cls.category_model = cls.env["res.partner.category"]
         cls._create_test_terms()
 
     @classmethod
@@ -21,17 +21,16 @@ class TestTranslationFrCa(TransactionCase):
         })
 
     def test_translation_applied_to_record_field(self):
-        partner = self._create_test_partner("Voici votre facture")
-        partner.action_translate_to_ca()
-        ca_value = partner.with_context(lang="fr_CA").comment
+        category = self._create_test_category("Voici votre facture")
+        category.action_translate_to_ca()
+        ca_value = category.with_context(lang="fr_CA").name
         self.assertEqual(ca_value, "Voici votre facture canadienne")
 
     def test_global_mapping_retrieval(self):
-        mapping = self.term_model.get_mapping_for_model("res.partner")
+        mapping = self.term_model.get_mapping_for_model("res.partner.category")
         self.assertEqual(mapping.get("facture"), "facture canadienne")
 
-    def _create_test_partner(self, comment_text):
-        return self.partner_model.with_context(lang="fr_FR").create({
-            "name": "Test Partner",
-            "comment": comment_text,
+    def _create_test_category(self, name_text):
+        return self.category_model.with_context(lang="fr_FR").create({
+            "name": name_text,
         })
